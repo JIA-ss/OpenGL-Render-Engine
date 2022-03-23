9f1ab2486f72c3098dc178ae3cb07aeb66d5eab3
s-Joshua-s <627723154@qq.com>
2022-03-23 15:59:14 +0800

    transform default value
    Signed-off-by: s-Joshua-s <627723154@qq.com>

--- src/component/Transform.h
+++ src/component/Transform.h
@@ -9,7 +9,7 @@ COMPONENT_NAMESPACE_BEGIN
 class sTransform: public sComponent
 {
     COMPONENT_DECLARE
-    COMPONENT_PROPERTY(glm::vec3, position, glm::vec3(1.0f))
+    COMPONENT_PROPERTY(glm::vec3, position, glm::vec3(0.0f))
     COMPONENT_PROPERTY(sTransform*, parent, nullptr);
     COMPONENT_PROPERTY(std::vector<sTransform*>, children, {});
 };
