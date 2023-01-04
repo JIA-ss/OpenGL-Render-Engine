cd13dc4f4c9de1bf11431b9ec02197185aa1fb79
joshuasun <joshuasun@lilith.com>
2023-01-04 13:35:00 +0800

    run

--- src/component/UITransform.cpp
+++ src/component/UITransform.cpp
@@ -42,21 +42,21 @@ void sUITransform::UpdateModelMatriceRecursively()
     m_dirty = false;
     m_globalPosition = m_position;
 
-    glm::mat4 parentModel(1.0f);
-    if (m_parent)
-    {
-        parentModel = m_parent->get_modelMatrice();
-        m_globalPosition += m_parent->getGlobalPosition();
-    }
-
-    m_modelMatrice = glm::mat4(1.0f);
-    m_modelMatrice = glm::scale(m_modelMatrice, m_size);
-    m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.x), glm::vec3(1,0,0));
-    m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.y), glm::vec3(0,1,0));
-    m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.z), glm::vec3(0,0,1));
-    m_modelMatrice = glm::translate(m_modelMatrice, m_position);
-
-    m_modelMatrice *= parentModel;
+    //glm::mat4 parentModel(1.0f);
+    //if (m_parent)
+    //{
+    //    parentModel = m_parent->get_modelMatrice();
+    //    m_globalPosition += m_parent->getGlobalPosition();
+    //}
+//
+    //m_modelMatrice = glm::mat4(1.0f);
+    //m_modelMatrice = glm::scale(m_modelMatrice, m_size);
+    //m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.x), glm::vec3(1,0,0));
+    //m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.y), glm::vec3(0,1,0));
+    //m_modelMatrice = glm::rotate(m_modelMatrice, glm::radians(m_rotation.z), glm::vec3(0,0,1));
+    //m_modelMatrice = glm::translate(m_modelMatrice, m_position);
+//
+    //m_modelMatrice *= parentModel;
 
     for (auto& child : m_children)
     {
